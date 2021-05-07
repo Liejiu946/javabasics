f918ef6e3c9b9d780eea250afbceb33d007601fc
liejiu946 <Liejiu946@163.com>
2021-05-07 17:21:38 +0800

    new project

---
+++ src/com/learn/grammar/grammar_01.java
@@ -0,0 +1,26 @@
+package com.learn.grammar;
+
+public class grammar_01 {
+        /*基础语法
+        * 1.基本数据类型
+        * 分为：整型、浮点型、字符、布尔
+        * String不属于基本数据类型*/
+
+        /*整型*/
+        byte aByte = 0;
+        short aShort = 0;
+        int anInt = 0;
+        long aLong = 1L;
+        /*浮点型*/
+        float aFloat = 0;
+        double aDouble = 0.1;
+        /*字符型*/
+        char aChar = 'a';
+        /*布尔*/
+        boolean aBoolean = false;
+        /*String是一个类*/
+        String string = "abc";
+
+        /*基本数据类型的分装类*/
+        Byte aByte2 = 0;
+}
